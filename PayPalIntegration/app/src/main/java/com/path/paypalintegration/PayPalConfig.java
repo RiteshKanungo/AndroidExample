ee1717685938d16708f5501a33f20cbb836f4cb8
path <path@gmauil.com>
2019-09-11 12:11:47 +0530

    PayPal Integration

---
+++ PayPalIntegration/app/src/main/java/com/path/paypalintegration/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.path.paypalintegration;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "Client_id ATSHe1IYyl7VirDf1MVgFwt0lkF263i0YMhaFJWBFI9aW-Fpi1-RM_LopxGTZ1ygFe7ABYiGKU_6SZjo";
+}
